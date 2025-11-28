50b225eb3096d96a771f18a7a4ca2aa015ae1c37
roanalvrz <roanalvrz@gmail.com>
2025-11-28 17:53:14 +0800

    Updated effects

--- lib/LightingModes/LightingModes.cpp
+++ lib/LightingModes/LightingModes.cpp
@@ -1,30 +1,42 @@
 #include "LightingModes.h"
 #include <Arduino.h>
-#define LED_PIN    3   //GPIO 3 for ESP32-C3; GPIO 13 for NodeMCU-32S; or any PWM pin
+#define LED_PIN    13   //GPIO 3 for ESP32-C3; GPIO 13 for NodeMCU-32S; or any PWM pin
 
 // Lighting is driven by MQTT messages — no periodic blinking here.
 
 void alert() { //PWM alert "soft blink"
   // increase the LED brightness
-  for(int i = 0; i <= 150; i++){   
+  for(int i = 0; i <= 255; i++){   
     // changing the LED brightness with PWM
     analogWrite(LED_PIN, i);
     delay(15);
   }
   // decrease the LED brightness
-  for(int i = 150; i >= 0; i--){
+  for(int i = 255; i >= 0; i--){
     // changing the LED brightness with PWM
     analogWrite(LED_PIN, i);
     delay(15);
   }
   // increase the LED brightness
-  for(int i = 0; i <= 150; i++){   
+  for(int i = 0; i <= 255; i++){   
     // changing the LED brightness with PWM
     analogWrite(LED_PIN, i);
     delay(15);
   }
   // decrease the LED brightness
-  for(int i = 150; i >= 0; i--){
+  for(int i = 255; i >= 0; i--){
+    // changing the LED brightness with PWM
+    analogWrite(LED_PIN, i);
+    delay(15);
+  }
+  // increase the LED brightness
+  for(int i = 0; i <= 255; i++){   
+    // changing the LED brightness with PWM
+    analogWrite(LED_PIN, i);
+    delay(15);
+  }
+  // decrease the LED brightness
+  for(int i = 255; i >= 0; i--){
     // changing the LED brightness with PWM
     analogWrite(LED_PIN, i);
     delay(15);
@@ -32,12 +44,13 @@ void alert() { //PWM alert "soft blink"
 }
 
 void standby() {
-  analogWrite(LED_PIN, 150); // full brightness LED to indicate standby mode
+  analogWrite(LED_PIN, 0); // full brightness LED to indicate standby mode
 }
 
 void LEDsetup() {
   pinMode(LED_PIN, OUTPUT);
-  analogWrite(LED_PIN, 255); // Start with LED on HIGH for 2 seconds
-  delay(2000);
-  analogWrite(LED_PIN, 150); // then set to standby brightness
+  analogWrite(LED_PIN, 0); // Start with LED on HIGH for 2 seconds
+  //delay(2000);
+  //analogWrite(LED_PIN, HIGH); // then set to standby brightness
+  //analogWrite(LED_PIN, LOW); // Start with LED on HIGH for 2 seconds
 }
